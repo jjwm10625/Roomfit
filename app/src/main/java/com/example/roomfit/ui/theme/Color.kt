c297f297e507009914817332efaa94d46502ac48
jjwm10625 <choyeongseo950@gmail.com>
2024-11-21 11:18:55 +0900

    [SETTING] 컬러 시스템, 폰트 설정

--- app/src/main/java/com/example/roomfit/ui/theme/Color.kt
+++ app/src/main/java/com/example/roomfit/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.example.roomfit.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BackgroundBeige = Color(0xFFE5E3DD)
+val White = Color(0xFFFAFAFA)
+val BtnBeige = Color(0xFFD0CDC2)
+val Gray = Color(0xFFB5B5BE)
+val Black = Color(0xFF000000)
+val BtnBlack = Color(0xFF241C1C)
+val OffWhite = Color(0xFFF5F4F2)
+val ComponentBeige = Color(0xFF959182)
--- app/src/main/java/com/example/roomfit/ui/theme/Type.kt
+++ app/src/main/java/com/example/roomfit/ui/theme/Type.kt
@@ -2,9 +2,13 @@ package com.example.roomfit.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.roomfit.R
+
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,20 +19,51 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
+
+
+val pretendardBold = FontFamily(Font(R.font.pretendard_bold, FontWeight.Bold))
+val pretendardSemiBold = FontFamily(Font(R.font.pretendard_semibold, FontWeight.SemiBold))
+val pretendardRegular = FontFamily(Font(R.font.pretendard_regular, FontWeight.Normal))
+
+var buttonLarge = TextStyle(
+    fontFamily = pretendardBold,
+    fontSize = 45.sp
+)
+
+var buttonSmall = TextStyle(
+    fontFamily = pretendardBold,
+    fontWeight = FontWeight.Bold,
+    fontSize = 40.sp
+)
+
+var bodyName = TextStyle(
+    fontFamily = pretendardBold,
+    fontWeight = FontWeight.Bold,
+    fontSize = 48.sp
+)
+
+var bodyLarge = TextStyle(
+    fontFamily = pretendardRegular,
+    fontWeight = FontWeight.Normal,
+    fontSize = 40.sp
+)
+
+var bodyBold = TextStyle(
+    fontFamily = pretendardBold,
+    fontWeight = FontWeight.Bold,
+    fontSize = 40.sp
+)
+
+var bodyDetail = TextStyle(
+    fontFamily = pretendardSemiBold,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 35.sp
+)
+
+var bodyWriting = TextStyle(
+    fontFamily = pretendardRegular,
+    fontWeight = FontWeight.Normal,
+    fontSize = 35.sp
+)
+
