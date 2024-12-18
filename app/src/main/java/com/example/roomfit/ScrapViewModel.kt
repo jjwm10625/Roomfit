2a795ad21cae5825d75374653e400e510e95b21e
chaehyeon03 <jennykim0114@naver.com>
2024-12-19 00:49:18 +0900

    [FEAT] 내 찜 목록 viewmodel 적용

---
+++ app/src/main/java/com/example/roomfit/ScrapViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.roomfit
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+
+data class ScrapItem(
+    val userName: String,
+    val postTitle: String,
+    val postContent: String,
+    val profileImageRes: Int,
+    val routeKey: String
+)
+
+
+class ScrapViewModel : ViewModel() {
+    val scrapList = mutableStateListOf<ScrapItem>()
+
+    fun addScrap(item: ScrapItem) {
+        if (!scrapList.contains(item)) {
+            scrapList.add(item)
+        }
+    }
+}
