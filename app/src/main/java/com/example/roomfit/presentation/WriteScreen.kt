5945e2aaf61fbc703b352edc221aba6827fd6d22
chaehyeon03 <jennykim0114@naver.com>
2024-12-19 16:47:02 +0900

    [FEAT] 사람을 구해요, 방을 구해요 값 저장

--- app/src/main/java/com/example/roomfit/presentation/WriteScreen.kt
+++ app/src/main/java/com/example/roomfit/presentation/WriteScreen.kt
@@ -14,9 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,7 +26,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -77,7 +74,6 @@ fun WriteScreen(
             .fillMaxSize()
             .background(BackgroundBeige)
             .padding(16.dp)
-            //.verticalScroll(rememberScrollState())
     ) {
         // 글 작성하기
         Text(
